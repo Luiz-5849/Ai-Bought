0b954c0f607b2bbebee8d4a2d976238bd68f1ca2
Luiz-5849 <luizhenriqueemilianofeitoza@gmail.com>
2022-03-30 21:43:11 -0300

    Tentativa de utilizar o express

--- src/datasource/index.js
+++ src/datasource/index.js
@@ -4,10 +4,34 @@ const axios = require('axios');
 const linkProviderBR = 'http://616d6bdb6dacbb001794ca17.mockapi.io/devnology/brazilian_provider';
 const linkProviderEU = 'http://616d6bdb6dacbb001794ca17.mockapi.io/devnology/european_provider';
 
-export const apiB = axios.create({
+const apiB = axios.create({
   baseURL: linkProviderBR
 });
 
-export const apiE = axios.create({
+const apiE = axios.create({
   baseURL: linkProviderEU
 });
+
+// const lh = axios.create({
+//   baseURL: "http://localhost:8000/produtos"
+// })
+
+module.exports = {apiB, apiE}
+
+// const server = express();
+// server.use(express.json());
+// server.listen(8000)
+
+// server.get("/produtos", async (req, res) => {
+//   try {
+//     const { data } = await apiB.get('/')
+//     const response = await apiE.get('/')
+//     const products = [...data, ...response.data].map((product, index) => ({
+//       ...product,
+//       id: index + 1
+//     }))
+//     res.send(products)
+//   } catch (error) {
+//     console.error(error.message)
+//   }
+// })
