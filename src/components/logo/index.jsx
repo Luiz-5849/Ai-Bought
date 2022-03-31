01975242e2f793ea56e988182ba12655bc1d9367
Luiz-5849 <luizhenriqueemilianofeitoza@gmail.com>
2022-03-30 21:48:09 -0300

    Criação de função de redirect ao clicar no logo

--- src/components/logo/index.jsx
+++ src/components/logo/index.jsx
@@ -1,8 +1,13 @@
 import logo from '../../assets/img/Vector.svg';
 
 export default function Logo () {
+
+  function redirect () {
+    window.location.href = "/"
+  }
+
   return (
-    <div className="logo">
+    <div className="logo" onClick={redirect} >
       <img src={logo} alt="logo" className="logo__img" />
       <span className="logo__text-alura">Ai!</span><span className="logo__text-geek">Bought</span>
     </div>
